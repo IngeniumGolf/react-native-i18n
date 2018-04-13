df07f7b594e0f50e8746aabb0b881c863fd2f527
plougsgaard <thomas@plougsgaard.dk>
2018-04-13 13:49:05 +0200

    Hack it.

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const RNI18n = require('react-native').NativeModules.RNI18n;
 const I18nJs = require('i18n-js');
 
 if (typeof RNI18n !== 'undefined') {
-  I18nJs.locale = RNI18n.languages[0];
+  I18nJs.locale = RNI18n.locale || 'en';
 } else if (__DEV__) {
   console.warn('react-native-i18n module is not correctly linked');
+  I18nJs.locale = 'en'
 }
 
-export const getLanguages = () => RNI18n.getLanguages();
+export const getLanguages = () => []; // not supported
 export default I18nJs;
